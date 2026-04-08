86a7f8f0cf513c6bbcc4fe8d5fccba9386ea44f7
irenealesya248214-dev <irenealesya248214@student.smkn1kandeman.sch.id>
2026-04-08 10:25:12 +0700

    halaman5.php

--- halaman5.php
+++ halaman5.php
@@ -1,1 +1,181 @@
-<h1>halaman 5<h1>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Deskripsi Kandidat</title>
+<style>
+
+body{
+    font-family: Arial, sans-serif;
+    background: #f5f5f5;
+    margin:0;
+}
+
+.main{
+    background:#E8F2F8;
+    min-height:100vh;
+    padding:40px;
+}
+
+.container{
+    display:flex;
+}
+
+.sidebar{
+    width:220px;
+    background:#3f5d7d;
+    color:white;
+    height:100vh;
+    padding:20px;
+}
+
+.sidebar h3{
+    font-size:14px;
+    margin-bottom:30px;
+}
+
+.menu{
+    margin-top:40px;
+}
+
+.menu a{
+    display:block;
+    color:white;
+    text-decoration:none;
+    margin:15px 0;
+}
+
+.main{
+    flex:1;
+    padding:40px;
+}
+
+.title{
+    text-align:center;
+    font-size:28px;
+    font-weight:bold;
+    margin-bottom:30px;
+}
+
+.card{
+    background:white;
+    border-radius:10px;
+    padding:30px;
+    width:750px;
+    margin:auto;
+    box-shadow:0 0 10px rgba(0,0,0,0.1);
+}
+
+.content{
+    display:flex;
+    gap:25px;
+}
+
+.photo img{
+    width:180px;
+    height: 90;
+    border-radius:8px;
+}
+
+.info h2{
+    margin:0;
+}
+
+.info p{
+    font-size:14px;
+}
+
+.button-area{
+    margin-top:20px;
+    text-align: center;
+    
+}
+
+button{
+    padding:10px 18px;
+    border:none;
+    border-radius:6px;
+    cursor:pointer;
+    margin: 0 25px;
+}
+
+.vote{
+    background:#6c8fb3;
+    color:white;
+}
+
+.back{
+    background:#4a6d90;
+    color:white;
+}
+
+</style>
+</head>
+
+<body>
+
+<div class="container">
+
+<div class="sidebar">
+
+<h3>SISTEM PEMILIHAN<br>KEPALA DESA</h3>
+
+<div class="menu">
+<a href="#">VOTING</a>
+<a href="#">HASIL VOTING</a>
+</div>
+
+</div>
+
+
+<div class="main">
+
+<div class="title">
+DESKRIPSI KANDIDAT 3
+</div>
+
+<div class="card">
+
+<div class="content">
+
+<div class="photo">
+<img src=>
+</div>
+
+<div class="info">
+
+<h2>JAMAL</h2>
+<p>Calon Kepala Desa 2026-2031</p>
+
+<hr>
+
+<h3>Visi:</h3>
+<p>
+Mewujudkan desa yang maju, mandiri, sejahtera, dan harmonis
+melalui pelayanan yang adil, transparan, dan berkelanjutan.
+</p>
+
+<h3>Misi:</h3>
+<ol>
+<li>Meningkatkan pelayanan publik yang cepat dan transparan.</li>
+<li>Mengembangkan potensi desa dan ekonomi masyarakat.</li>
+<li>Membangun infrastruktur desa secara merata.</li>
+<li>Meningkatkan kesejahteraan dan partisipasi masyarakat.</li>
+<li>Menjaga keamanan, budaya, dan kerukunan warga.</li>
+</ol>
+
+<div class="button-area">
+<button class="vote">Vote Sekarang</button>
+<button class="back">Kembali</button>
+</div>
+
+</div>///
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
