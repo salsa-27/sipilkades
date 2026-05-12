6b3993c5d422da18a19c1f30a131a745047a2f8e
azhraa-ahra <raniazzahra248225@student.smkn1kandeman.sch.id>
2026-05-12 11:25:05 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Hasil Voting</title>
+<!-- Link icon dari Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    background-color: #d0d7dc; 
+  }
+
+  .sidebar {
+    width: 200px;
+    background-color: #2c3e50;
+    height: 100vh;
+    padding: 20px;
+    box-sizing: border-box;
+    position: fixed;
+  }
+
+  .sidebar h2 {
+    color: #fff;
+    margin-bottom: 30px;
+    font-size: 16px;
+  }
+
+  .sidebar a {
+    display: block;
+    color: #ecf0f1;
+    padding: 10px 0;
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  .main {
+    margin-left: 200px;
+    padding: 20px;
+  }
+
+  h1 {
+    text-align: center;
+    color: #2c3e50;
+    margin-bottom: 30px;
+  }
+ 
+  .cards {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    background-color: #fff;
+    width: 200px;
+    padding: 10px;
+    border-radius: 10px;
+    text-align: center;
+    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  }
+
+  .card img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-bottom: 10px;
+  }
+
+  .name {
+    font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+  .vote {
+    font-weight: bold;
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+
+  .percentage {
+    font-size: 14px;
+    color: #555;
+  }
+
+ 
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 160px;
+    padding: 10px;
+    color: #fff;
+    text-align: center;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+</style>
+</head>
+<body>
+
+<div class="sidebar">
+  <h2>SISTEM PEMILIHAN KEPALA DESA</h2>
+  <a href="#">VOTING</a>
+  <a href="#">HASIL VOTING</a>
+  <div class="logout">LOG OUT</div>
+</div>
+
+<div class="main">
+  <h1>HASIL VOTING</h1>
+  <div class="cards">
+    
+    <div class="card img">
+      <img src="images/agus" alt= "Agus"/>
+      <div class="name">AGUS</div>
+      <div class="vote">0% <br> 0 Votes</div>
+    </div>
+    
+    <div class="card img">
+      <img src="images/bagas" alt="Bagas"/>
+      <div class="name">BAGAS</div>
+      <div class="vote">0% <br> 0 Votes</div>
+    </div>
+    
+    <div class="card img">
+      <img src="images/jamals" alt="Jamal"/>
+      <div class="name">JAMAL</div>
+      <div class="vote">0% <br> 0 Votes</div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
